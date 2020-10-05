b7e1eca92978bd33f5bed8951b2d78cfcde66865
W.R. Jackson <jackson@justjackson.dev>
2020-10-05 19:30:43 -0400

    Timestamps, median intensity, etc

--- file_utilities/nd2_to_giff.py
+++ file_utilities/nd2_to_giff.py
@@ -16,24 +16,34 @@ How to Use:
 Written by W.R. Jackson <wrjackso@bu.edu>, DAMP Lab 2020
 --------------------------------------------------------------------------------
 '''
-import glob
-import os
-import time
+import datetime
 import warnings
+from typing import (
+    List,
+)
 
 import click
 import cv2
+import imageio
 import nd2reader
 import numpy as np
 import tqdm
-from pims import ND2_Reader
-from skimage import filters
-from skimage.io import imsave as png_save
-from tiffile import imsave as tiff_save
-import imageio
+from PIL import (
+    ImageFont,
+)
 
 warnings.filterwarnings("ignore", category=UserWarning)
 warnings.filterwarnings("ignore", category=RuntimeWarning)
+warnings.filterwarnings("ignore")
+
+
+# ---------------------------- Utility Functions -------------------------------
+def dtype_conversion_frame(
+        input_image: np.ndarray,
+        datatype: str = 'uint8',
+):
+    if datatype == 'uint8':
+        return input_image.astype(np.uint8)
 
 
 def colorize_frame(input_image, color):
@@ -42,7 +52,121 @@ def colorize_frame(input_image, color):
         input_image[:, :, (0, 2)] = 0
     if color == 'red':
         input_image[:, :, (1, 2)] = 0
+
     return input_image
+
+
+# Modify the `contrast` value below to change the contrast of the input
+# pictures.
+def modify_contrast(
+        input_image, alpha: int = 5,
+        contrast: int = 127,
+):
+    '''
+
+    Args:
+        input_image:
+        alpha:
+        contrast:
+
+    Returns:
+
+    '''
+    return cv2.addWeighted(
+        input_image,
+        alpha,
+        input_image,
+        0,
+        contrast,
+    )
+
+
+# -------------------------------- Statistics ----------------------------------
+
+
+def generate_summary_statistics(
+        timing_information: datetime.datetime,
+        cherry_frame: np.ndarray,
+        yfp_frame: np.ndarray,
+        frame_index: int,
+):
+    out_list = []
+    minute_increment = frame_index // 10
+    d1 = timing_information + datetime.timedelta(minutes=minute_increment*15)
+    # Figure out how to make this more human readable. HH:MM?
+    out_list.append(f'{d1}')
+    # Segmentation Count goes here?
+    out_list.append(f'Frame Index {frame_index}')
+    out_list.append(f'Green Mean Intensity: {int(np.mean(cherry_frame))}')
+    out_list.append(f'Red Mean Intensity: {int(np.mean(yfp_frame))}')
+    return out_list
+
+
+def histogram_visualization():
+    '''
+
+    Returns:
+
+    '''
+    pass
+
+
+def write_summary_statistics(
+        input_frame: np.ndarray,
+        input_stats: List[str],
+        position: str = 'mid',
+        font_name: str = 'Hack.ttf',
+        font_size: int = 12,
+):
+    initial_position = [0, 0]
+    array_shape_x, array_shape_y = input_frame.shape[0], input_frame.shape[1]
+    downward_text = True if position in ('tl', 'tr') else False
+    # Determine our maximum line length. Assuming that requested text is
+    # centered.
+    max_length = 0
+    max_string = None
+    for line in input_stats:
+        if len(line) > max_length:
+            max_length = len(line)
+            max_string = line
+    # Convert our characters into pixels for the purposes of offsetting the
+    # origin.
+    font = ImageFont.truetype(font_name, font_size)
+    max_string_width, line_height = font.getsize(max_string)
+    string_heights = max_string_width * len(input_stats)
+    offset_buffers = array_shape_x / 10, array_shape_y / 10
+    if position == 'tl':
+        initial_position[0] = initial_position[0] + offset_buffers[0]
+        initial_position[1] = initial_position[1] + offset_buffers[1]
+    if position == 'mid':
+        initial_position[0] = array_shape_x / 2 - max_string_width
+        initial_position[1] = array_shape_y / 2 + string_heights
+    if position == 'tr':
+        initial_position[0] = (array_shape_x - offset_buffers[0]) - max_length
+        initial_position[1] = array_shape_y + offset_buffers[1]
+    if position == 'br':
+        initial_position[0] = (array_shape_x - offset_buffers[0]) - max_length
+        initial_position[1] = array_shape_y + string_heights + offset_buffers[1]
+    if position == 'bl':
+        initial_position[0] = initial_position[0] + offset_buffers[0]
+        initial_position[1] = array_shape_y + string_heights + offset_buffers[1]
+    y_track = 100
+    out_frame = input_frame
+    for line in input_stats:
+        line_width = font.getsize(line)[0]
+        _x_pos = (initial_position[0] - line_width) / 2
+        cv2.putText(
+            out_frame,
+            line,
+            (100, y_track),
+            cv2.FONT_HERSHEY_SIMPLEX,
+            1,
+            (255, 255, 255),
+            6,
+            cv2.LINE_AA,
+        )
+        y_track = y_track + 30
+    return out_frame
 
 
 @click.command()
@@ -52,77 +176,55 @@ def colorize_frame(input_image, color):
     help='The path to the ND2 files to be converted.',
     default='/media/jackson/WD_BLACK/microscopy_images/to_gif/10_h.nd2',
 )
-@click.option(
-    '--output_file',
-    prompt='Output directory for converted TIFF Files',
-    help='The path to the ND2 files to be converted.',
-    default='../test',
-)
-def make_gif(input_file: str, output_file: str):
+
+def make_gif(input_file: str):
     with nd2reader.ND2Reader(input_file) as input_frames:
-        # Available dimensions are:
-        #   - x: Width
-        #   - y: Height
-        #   - c: Channel 3
-        #   - t: Time 30
-        #   - m: Movie? 9
-        # Should be 810 frames.
         frame_count = input_frames.sizes['t']
         channels = input_frames.sizes['c']
-        fov_count = input_frames.sizes['v']
         input_frames.iter_axes = 'vtc'
-        file_name_list = []
-        # Take in the image
-        # Iterate over each view, and if that view is blank skip over
-        # it.
-        # If the view is real, we overlay both green and red channels
-        # over the grayscale image.
-        # We then append the resultant image to a list
-        # Once we've iterated over the sum of the entire image, we
-        # collapse it into a gif.
-        # Start: 0
-        # Stop: 288
-        # Start two: 360 ish
-        # Stop 2: 729 ish
-        # Start: 1089
-        # Stop: 1452
-        # Start: 1815
-        # Stop: 2172
-        # Start: 2537
-        # Stop: 2901
-        # Start: 3261
-        # Start:  3627
-        observation_start_stop = [
-            [0, 288, 9, 'test_1'],
-            [288, 360, 9, 'test_2'],
-            [360, 729, 9, 'test_3'],
-            [729, 1089, 9, 'test_4'],
-            [1089, 1452, 9, 'test_5'],
-            [1452, 1815, 9, 'test_6'],
-            [2172, 2537, 9, 'test_7'],
-            [2901, 3261, 9, 'test_8'],
-            [3261, 3627, 9, 'test_9'],
-        ]
-        for start, stop, stride, label in observation_start_stop:
+        observation_start_stop = []
+        number = frame_count * channels
+        print(input_frames.get_timesteps())
+        for observation in range(number-1):
+            observation_start_stop.append(
+                [
+                    (observation * number),
+                    (observation + 1) * number,
+                    channels * channels,
+                    f'{input_file.split("/")[-1][:-3]}_{observation}',
+                ]
+            )
+        events = input_frames.events
+        for start, stop, stride, label in tqdm.tqdm(observation_start_stop):
             interior_list = []
             for frame_index in range(start, stop, stride):
+                grey_frame = input_frames[frame_index]
+                cherry_frame = input_frames[frame_index + 1]
+                yfp_frame = input_frames[frame_index + 2]
+                date = input_frames.metadata['date']
+                stats_list = generate_summary_statistics(
+                    date,
+                    cherry_frame,
+                    yfp_frame,
+                    frame_index,
+                )
                 grey_frame = colorize_frame(
-                    input_frames[frame_index],
+                    grey_frame,
                     False,
                 )
-                cherry_frame = colorize_frame(
-                    input_frames[frame_index + 1],
+                cherry_frame = modify_contrast(colorize_frame(
+                    cherry_frame,
                     'green',
-                )
-                yfp_frame = colorize_frame(
-                    input_frames[frame_index + 2],
+                ))
+                yfp_frame = modify_contrast(colorize_frame(
+                    yfp_frame,
                     'red',
-                )
+                ))
                 intermediate_frame = cv2.addWeighted(
                     grey_frame,
                     1,
                     cherry_frame,
-                    0.76,
+                    0.75,
                     0,
                 )
                 out_frame = cv2.addWeighted(
@@ -132,19 +234,13 @@ def make_gif(input_file: str, output_file: str):
                     0.75,
                     0,
                 )
-                position = (
-                    int(out_frame.shape[1] / 2 - 268 / 2),
-                    int(out_frame.shape[0] / 2 - 36 / 2)
+                # out_frame = dtype_conversion_frame(out_frame)
+                out_frame = write_summary_statistics(
+                    out_frame,
+                    stats_list,
                 )
-                cv2.putText(
-                    out_frame,  # numpy array on which text is written
-                    f'{frame_index % stride}',  # text
-                    position,  # position at which writing has to start
-                    cv2.FONT_HERSHEY_DUPLEX,  # font family
-                    1,  # font size
-                    (209, 80, 0, 255),  # font color
-                    3)  # font stroke
                 interior_list.append(out_frame)
+            print(f'{label}.gif')
             imageio.mimsave(f'{label}.gif', interior_list)
 
 
